8612f1ee8fccbe866cf791668d8bc5632b9e1735
Beau <beaudenheijer@gmail.com>
2021-08-05 01:05:27 +0200

    feat(client): updated settings page

--- packages/vindigo-client/src/views/settings/Settings.vue
+++ packages/vindigo-client/src/views/settings/Settings.vue
@@ -1,21 +1,45 @@
 <template>
 	<section class="settings-page">
 		<toolbar class="pl-0" />
-		<main class="container mt-4" role="main">
-			<w-tabs
-				:items="settingTabs"
-				center
-				:transition="false"
-				:fill-bar="true"
-			>
-				<template #item-content="{ item }">
-					<section class="container">
-						<div class="py-5">
-							<component :is="item.content" />
-						</div>
-					</section>
-				</template>
-			</w-tabs>
+		<section class="bg-white pb-10 dark:bg-gray-800">
+			<div class="container flex items-center">
+				<section class="flex items-center py-10">
+					<avatar
+						class="mr-4"
+						:profile="$vuex.state.profile"
+						:size="'80px'"
+					/>
+					<div class="settings-page__profile-details">
+						<h1 class="block text-3xl">
+							{{ $vuex.state.profile.fullName }}
+						</h1>
+						<p>{{ $t('SETTINGS_PROFILE_DESCRIPTION') }} </p>
+					</div>
+				</section>
+				<spacer />
+				<section>
+					<w-button>
+						{{ $t('SETTINGS_PROFILE_WATCH_PROFILE' ) }}
+					</w-button>
+				</section>
+			</div>
+		</section>
+		<main class="mt-[-58px]" role="main">
+			<section class="container">
+				<w-tabs
+					:items="settingTabs"
+					:transition="false"
+					:fill-bar="true"
+				>
+					<template #item-content="{ item }">
+						<section class="rounded-md mt-5 p-8 bg-white dark:bg-gray-800">
+							<component
+								:is="item.content"
+							/>
+						</section>
+					</template>
+				</w-tabs>
+			</section>
 		</main>
 	</section>
 </template>
@@ -26,9 +50,14 @@ import AppearenceTab from "./tabs/AppearenceTab.vue";
 import GeneralTab from "./tabs/GeneralTab.vue";
 import PrivacyTab from "./tabs/PrivacyTab.vue";
 import AccountTab from "./tabs/AccountTab.vue";
+import Avatar from "../../components/Avatar.vue";
 
 export default Vue.extend({
 	name: "VindigoSettings",
+	components: { Avatar },
+	data: () => ({
+		data: undefined
+	}),
 	computed: {
 		settingTabs() {
 			return [
@@ -54,8 +83,18 @@ export default Vue.extend({
 	.w-tabs {
 		@apply border-none;
 
+		&__content {
+			@apply px-0;
+		}
+
 		&__content-wrap {
-			@apply rounded-b-md bg-white dark:bg-gray-800;
+			@apply rounded-b-md;
+		}
+	}
+
+	&__profile-details {
+		h1, p {
+			line-height: 28px;
 		}
 	}
 }
