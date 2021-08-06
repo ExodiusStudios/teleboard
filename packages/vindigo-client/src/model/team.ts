d6895a646134ea427f21f8a5db8e31c8b9c25154
macjuul <julian.exodius@gmail.com>
2021-08-06 03:43:38 +0200

    feat: render team icons

---
+++ packages/vindigo-client/src/model/team.ts
@@ -0,0 +1,12 @@
+/**
+ * Data representing a team
+ */
+export interface Team {
+	id: number
+	slug: string
+	name: string
+	description: string
+	createdAt: string
+	logoImage: string
+	teamUrl: string
+}
