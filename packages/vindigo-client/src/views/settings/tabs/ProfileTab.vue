0fffd8a336faed4c06e2ab4938c8b8ed0014356c
ItsMateo <mateotheofficial@gmail.com>
2021-08-08 01:27:43 -0400

    feat(client): move avatar uploading to settings

--- packages/vindigo-client/src/views/settings/tabs/ProfileTab.vue
+++ packages/vindigo-client/src/views/settings/tabs/ProfileTab.vue
@@ -1,47 +1,76 @@
 <template>
 	<div class="profile-page">
-		<w-input
-			v-model="fullname"
-			:placeholder="$t('GENERAL_FULL_NAME')"
-			tile inner-icon-left="mdi mdi-account"
-			inner-icon-right="mdi mdi-exclamation-thick text-red-500"
-		/>
+		<div class="profile-avatar">
+			<div class="avatar-upload">
+				Profile Picture
+				<file-upload 
+					v-model="avatar" 
+					class="avatarUpload" 
+					@preview="showPreview"
+				>
+					<w-button>
+						Change
+					</w-button>
+				</file-upload>
+				<avatar
+					v-if="preview == ''"
+					class="preview"
+					:profile="$vuex.state.profile"
+				/>
+				<img 
+					v-if="preview != ''"
+					class="preview" 
+					:src="preview"
+				>
+			</div>
+		</div>
 
-		<w-input
-			v-model="email"
-			class="mt-5"
-			:placeholder="$t('GENERAL_EMAIL')"
-			tile inner-icon-left="mdi mdi-mail"
-			inner-icon-right="mdi mdi-exclamation-thick text-red-500"
-		/>
+		<w-divider class="avatar-details-divider" />
+		
+		<div class="profile-details">
+			<w-input
+				v-model="fullname"
+				:placeholder="$t('GENERAL_FULL_NAME')"
+				tile inner-icon-left="mdi mdi-account"
+				inner-icon-right="mdi mdi-exclamation-thick text-red-500"
+			/>
 
-		<w-input
-			v-model="username"
-			class="mt-5"
-			:placeholder="$t('GENERAL_USERNAME')"
-			tile inner-icon-left="mdi mdi-account"
-			inner-icon-right="mdi mdi-exclamation-thick text-red-500"
-		/>
+			<w-input
+				v-model="email"
+				class="mt-5"
+				:placeholder="$t('GENERAL_EMAIL')"
+				tile inner-icon-left="mdi mdi-mail"
+				inner-icon-right="mdi mdi-exclamation-thick text-red-500"
+			/>
 
-		<w-textarea
-			v-model="bio"
-			class="mt-5"
-			:placeholder="$t('SETTINGS_PROFILE_BIO') + ' (Optional)'"
-		/>
- 
-		<w-input
-			v-model="website"
-			class="mt-5"
-			:placeholder="$t('SETTINGS_PROFILE_WEBSITE') + ' (Optional)'"
-			tile inner-icon-left="mdi mdi-earth"
-		/>
+			<w-input
+				v-model="username"
+				class="mt-5"
+				:placeholder="$t('GENERAL_USERNAME')"
+				tile inner-icon-left="mdi mdi-account"
+				inner-icon-right="mdi mdi-exclamation-thick text-red-500"
+			/>
 
-		<w-button class="mt-5 -ml-0" @click="saveUserProfile">
-			{{ $t("SETTINGS_PROFILE_SAVE") }}
-			<w-icon class="ml-2">
-				mdi mdi-content-save
-			</w-icon>
-		</w-button>
+			<w-textarea
+				v-model="bio"
+				class="mt-5"
+				:placeholder="$t('SETTINGS_PROFILE_BIO') + ' (Optional)'"
+			/>
+	
+			<w-input
+				v-model="website"
+				class="mt-5"
+				:placeholder="$t('SETTINGS_PROFILE_WEBSITE') + ' (Optional)'"
+				tile inner-icon-left="mdi mdi-earth"
+			/>
+
+			<w-button class="mt-5 -ml-0" @click="saveUserProfile">
+				{{ $t("SETTINGS_PROFILE_SAVE") }}
+				<w-icon class="ml-2">
+					mdi mdi-content-save
+				</w-icon>
+			</w-button>
+		</div>
 	</div>
 </template>
 
@@ -53,6 +82,8 @@ import { api } from '../../..';
 export default Vue.extend({
 	name: "ProfileTab",
 	data: () => ({
+		avatar: null,
+		preview: '',
 		fullname: '',
 		email: '',
 		username: '',
@@ -63,12 +94,19 @@ export default Vue.extend({
 		const profile = this.$vuex.state.profile;
 		
 		// assigning each profile variable
+		this.preview = profile!.avatar!;
 		this.fullname = profile!.fullName;
 		this.email = profile!.email!;
 		this.username = profile!.username;
 	},
 	methods: {
+		showPreview(preview: string) {
+			this.preview = preview;
+		},
 		async saveUserProfile() {
+			if(this.avatar != null) {
+				await api.upload('avatar', this.avatar!);
+			}
 			
 			const profile = {
 				fullname: this.fullname,
@@ -99,8 +137,24 @@ export default Vue.extend({
 </script>
 
 <style lang="postcss">
-	.profile-page {
+	.profile-avatar {
+		.preview {
+			width: 100px;
+			height: 100px;
+		}
+		.avatarUpload {
+			@apply flex-grow;
 
+			margin-top: 10px;
+			display: inline-block;
+			justify-self: right;
+			position: sticky;
+		}
+	}
+	.avatar-details-divider {
+		@apply my-6 h-1 bg-gray-200 dark:bg-gray-200;
+	}
+	.profile-details {
 		.w-input {
 			@apply rounded-md bg-light-3 px-3 outline-none overflow-hidden;
 
